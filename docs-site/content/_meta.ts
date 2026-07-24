898455a952825e5389e7ccedba33d85837f81bc7
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2026-07-24 18:25:51 +0600

    feat: integrate Nextra docs site and update external links

---
+++ docs-site/content/_meta.ts
@@ -0,0 +1,16 @@
+export default {
+  index: {
+    title: 'Introduction',
+    type: 'page',
+  },
+  installation: 'Installation',
+  features: 'Features',
+  configuration: 'Configuration',
+  keybindings: 'Keybindings',
+  'detail-view': 'Detail View',
+  'font-support': 'Font Support',
+  'status-indicators': 'Status Indicators',
+  panels: 'Panels',
+  readme: 'README',
+  changelog: 'Changelog',
+}
